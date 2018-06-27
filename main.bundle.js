47d1510b2a49ee864a4a3e3bb80080ca0c0f8d9a
pollockcl <pollock.clee@gmail.com>
2018-06-27 08:40:18 -0600

    changed base url

--- main.bundle.js
+++ main.bundle.js
@@ -414,7 +414,7 @@
 	  if (host === "localhost" || host === "127.0.0.1") {
 	    return "http://localhost:3000";
 	  } else {
-	    return "18.191.238.253";
+	    return "http://18.191.238.253";
 	  }
 	};
 
