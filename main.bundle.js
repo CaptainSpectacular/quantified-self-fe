e220953cb209102ebd7dcbea73447d45d304e9b6
pollockcl <pollock.clee@gmail.com>
2018-06-30 19:52:08 -0600

    changed baseurl

--- main.bundle.js
+++ main.bundle.js
@@ -414,7 +414,7 @@
 	  if (host === "localhost" || host === "127.0.0.1") {
 	    return "http://localhost:3000";
 	  } else {
-	    return "http://clee.kvstage.cloud";
+	    return "https://qs-be-js.herokuapp.com/";
 	  }
 	};
 
