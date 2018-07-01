ab78c27725edb0dc43e1666d167550e5ee653f38
pollockcl <pollock.clee@gmail.com>
2018-06-30 19:53:23 -0600

    changed again

--- main.bundle.js
+++ main.bundle.js
@@ -414,7 +414,7 @@
 	  if (host === "localhost" || host === "127.0.0.1") {
 	    return "http://localhost:3000";
 	  } else {
-	    return "https://qs-be-js.herokuapp.com/";
+	    return "https://qs-be-js.herokuapp.com";
 	  }
 	};
 
